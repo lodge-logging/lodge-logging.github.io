f55539b04c8e6ed731fb06df7d4bd866c2acdaf7
Sam Clark <clarksam19@gmail.com>
2022-01-05 10:44:51 -0600

    use gh-pages package

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -20,7 +20,7 @@ function Landing() {
       </div>
       <div className="hidden xl:flex flex-row h-4/6 bgImageSecond justify-center items-center text-center text-white bg-cyan py-20 z-40">
         <h2 className="text-4xl font-extrabold w-6/12">Ready to Deploy</h2>
-        <p className="text-3xl my-10 max-w-5xl w-6/12 max-w-full px-6">
+        <p className="text-3xl my-10 w-6/12 max-w-full px-6">
         Lodge is an opinionated pre-configuration of open-source tools
           that deploys automatically to the user's AWS account
         </p>
@@ -32,9 +32,9 @@ function Landing() {
           that deploys automatically to the user's AWS account
         </p>
       </div>
-      <div className="hidden xl:flex h-4/6 bgImageThird xl:flex-row xl:bgImageThird flex flex-col justify-center items-center text-center text-white bg-yellowgreen py-20">
+      <div className="xl:flex h-4/6 bgImageThird xl:flex-row xl:bgImageThird flex flex-col justify-center items-center text-center text-white bg-yellowgreen py-20">
         <h2 className="text-4xl font-extrabold w-6/12">Available and Resilient</h2>
-        <p className="text-3xl my-10 max-w-5xl w-6/12 max-w-full px-6">
+        <p className="text-3xl my-10 w-6/12 max-w-full px-6">
         Lodge is optimized for availability in the event of datacenter outages
           and resiliency to sudden bursts in log traffic
         </p>
