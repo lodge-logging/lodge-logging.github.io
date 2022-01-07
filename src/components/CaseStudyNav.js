3ba663cdf7065ae9610b908f8bc306d3d77d3104
rgdonovan <55225918+rgdonovan@users.noreply.github.com>
2022-01-07 18:41:46 +0100

    add EventHandlers
    Co-authored-by: Sam Clark <63471713+clarksam19@users.noreply.github.com>
    Co-authored-by: Justin Lo <50888159+justinnnlo@users.noreply.github.com>
    Co-authored-by: Rana Deeb <62123706+ranadeeb92@users.noreply.github.com>

--- src/components/CaseStudyNav.js
+++ src/components/CaseStudyNav.js
@@ -1,286 +1,137 @@
+import scrollToElement from "../scripts/scrollToElement";
 function CaseStudyNav() {
-  const linkStyle = "flex items-center my-4";
-  const subSectionStyle = "ml-8";
-  const textStyle = "inline text-raisin text-lg";
+  const listElementStyle = "my-4 hover:text-cyan";
+  const subSectionStyle = "ml-8 my-4 hover:text-cyan";
+  const textStyle = "inline text-left text-raisin text-lg hover:text-black";
   const navStyle = "sticky xl:flex xl:self-start xl:w-1/5 xl:top-16 right-20 bg-white border-gray-100 border-r-2 p-4 pl-14 h-screen overflow-y-auto";
+
+  const navigateTo = (elementId) => scrollToElement(elementId, -100);
 
   return (
     <aside id="sideNav" className={`${navStyle} translate-x transform transition duration-700 hidden h-0`}>
       <ul className="list-outside list-disc text-yellowgreen text-3xl items-center">
         {/* <!-- Section 1 --> */}
-        <li data-section="section-1">
-          <a href="#section-1" className={linkStyle}>
-            <p className={textStyle}>Lodge Introduction</p>
-          </a>
+        <li data-section="section-1" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-1")}>Lodge Introduction</button>
         </li>
-
         {/* <!-- Section 2 --> */}
-        <li data-section="section-2">
-          <a className={linkStyle} href="#section-2">
-            <div>
-              <p className={textStyle}>Lodge Use Case</p>
-            </div>
-          </a>
+        <li data-section="section-2" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-2")}>Lodge Use Case</button>
         </li>
         <li data-section="section-2" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-2-1">
-            <div>
-              <p className={textStyle}>Boardwalk</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-2-1")}>Boardwalk</button>
         </li>
         <li data-section="section-2" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-2-2">
-            <div>
-              <p className={textStyle}>Monolith to Microservices</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-2-2")}>Monolith to Microservices</button>
         </li>
         <li data-section="section-2" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-2-3">
-            <div>
-              <p className={textStyle}>Debugging Microservices</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-2-3")}>Debugging Microservices</button>
         </li>
         <li data-section="section-2" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-2-4">
-            <div>
-              <p className={textStyle}>Observability</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-2-4")}>Observability</button>
         </li>
         <li data-section="section-2" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-2-5">
-            <div>
-              <p className={textStyle}>Time-Series Data</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-2-5")}>Time-Series Data</button>
         </li>
         <li data-section="section-2" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-2-6">
-            <div>
-              <p className={textStyle}>Bursty Traffic</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-2-6")}>Bursty Traffic</button>
         </li>
         <li data-section="section-2" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-2-7">
-            <div>
-              <p className={textStyle}>Summary</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-2-7")}>Summary</button>
         </li>
-
         {/* <!-- Section 3 --> */}
-        <li data-section="section-3" className="">
-          <a className={linkStyle} href="#section-3">
-            <div>
-              <p className={textStyle}>Existing Solutions</p>
-            </div>
-          </a>
+        <li data-section="section-3" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-3")}>Existing Solutions</button>
         </li>
         <li data-section="section-3" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-3-1">
-            <div>
-              <p className={textStyle}>Buying a Solution</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-3-1")}>Buying a Solution</button>
         </li>
         <li data-section="section-3" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-3-2">
-            <div>
-              <p className={textStyle}>Operating a Solution</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-3-2")}>Operating a Solution</button>
         </li>
         <li data-section="section-3" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-3-3">
-            <div>
-              <p className={textStyle}>Where Lodge Fits In</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-3-3")}>Where Lodge Fits In</button>
         </li>
-
         {/* <!-- Section 4 --> */}
-        <li data-section="section-4">
-          <a className={linkStyle} href="#section-4">
-            <div>
-              <p className={textStyle}>Lodge Architecture</p>
-            </div>
-          </a>
+        <li data-section="section-4" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-4")}>Lodge Architecture</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-1">
-            <div>
-              <p className={textStyle}>Overview</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-1")}>Overview</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-2">
-            <div>
-              <p className={textStyle}>Components</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-2")}>Components</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-3">
-            <div>
-              <p className={textStyle}>Filebeat for Collection and Shipping</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-3")}>Filebeat for Collection and Shipping</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-4">
-            <div>
-              <p className={textStyle}>Kafka for Buffering</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-4")}>Kafka for Buffering</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-5">
-            <div>
-              <p className={textStyle}>Logstash for Parsing and Indexing</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-5")}>Logstash for Parsing and Indexing</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-6">
-            <div>
-              <p className={textStyle}>Elasticsearch and Amazon S3 for Storage</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-6")}>Elasticsearch and Amazon S3 for Storage</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-7">
-            <div>
-              <p className={textStyle}>Kibana for Querying and Visualization</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-7")}>Kibana for Querying and Visualization</button>
         </li>
         <li data-section="section-4" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-4-8">
-            <div>
-              <p className={textStyle}>Lodge Dashboard for Everything Else</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-4-8")}>Lodge Dashboard for Everything Else</button>
         </li>
-
         {/* <!-- Section 5 --> */}
-        <li data-section="section-5">
-          <a className={linkStyle} href="#section-5">
-            <div>
-              <p className={textStyle}>Using Lodge</p>
-            </div>
-          </a>
+        <li data-section="section-5" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-5")}>Using Lodge</button>
         </li>
         <li data-section="section-5" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-5-1">
-            <div>
-              <p className={textStyle}>Deploying Lodge</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-5-1")}>Deploying Lodge</button>
         </li>
         <li data-section="section-5" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-5-2">
-            <div>
-              <p className={textStyle}>Viewing the Deployment</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-5-2")}>Viewing the Deployment</button>
         </li>
         <li data-section="section-5" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-5-3">
-            <div>
-              <p className={textStyle}>Shipping Logs</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-5-3")}>Shipping Logs</button>
         </li>
         <li data-section="section-5" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-5-4">
-            <div>
-              <p className={textStyle}>Viewing Logs</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-5-4")}>Viewing Logs</button>
         </li>
         <li data-section="section-5" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-5-5">
-            <div>
-              <p className={textStyle}>Re-Indexing Logs from S3 Archive</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-5-5")}>Re-Indexing Logs from S3 Archive</button>
         </li>
         <li data-section="section-5" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-5-6">
-            <div>
-              <p className={textStyle}>Managing Kafka Cluster</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-5-6")}>Managing Kafka Cluster</button>
         </li>
         <li data-section="section-5" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-5-7">
-            <div>
-              <p className={textStyle}>Component Review</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-5-7")}>Component Review</button>
         </li>
-
         {/* <!-- Section 6 --> */}
-        <li data-section="section-6">
-          <a className={linkStyle} href="#section-6">
-            <div>
-              <p className={textStyle}>Designing and Building Lodge</p>
-            </div>
-          </a>
+        <li data-section="section-6" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-6")}>Designing and Building Lodge</button>
         </li>
         <li data-section="section-6" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-6-1">
-            <div>
-              <p className={textStyle}>Kafka</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-6-1")}>Kafka</button>
         </li>
         <li data-section="section-6" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-6-2">
-            <div>
-              <p className={textStyle}>Amazon S3</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-6-2")}>Amazon S3</button>
         </li>
         <li data-section="section-6" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-6-3">
-            <div>
-              <p className={textStyle}>Bastion Host</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-6-3")}>Bastion Host</button>
         </li>
         {/* <!-- Section 7 --> */}
-        <li data-section="section-7">
-          <a className={linkStyle} href="#section-7">
-            <div>
-              <p className={textStyle}>Implementation Challenge</p>
-            </div>
-          </a>
+        <li data-section="section-7" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-7")}>Implementation Challenge</button>
         </li>
         <li data-section="section-7" className={subSectionStyle}>
-          <a className={linkStyle} href="#section-7-1">
-            <div>
-              <p className={textStyle}>Resolving Circular Dependencies</p>
-            </div>
-          </a>
+          <button className={textStyle} onClick={() => navigateTo("section-7-1")}>Resolving Circular Dependencies</button>
         </li>
-
         {/* <!-- Section 8 --> */}
-        <li data-section="section-8">
-          <a className={linkStyle} href="#section-8">
-            <div>
-              <p className={textStyle}>Future Work</p>
-            </div>
-          </a>
+        <li data-section="section-8" className={listElementStyle}>
+          <button className={textStyle} onClick={() => navigateTo("section-8")}>Future Work</button>
         </li>
+        <div className="py-32" />
       </ul>
-    </aside >
+    </aside>
   );
 }
-
 export default CaseStudyNav;
